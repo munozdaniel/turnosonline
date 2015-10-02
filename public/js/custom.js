940a5204a64f20504b8b63b5b75ec58cbfe3358d
munozdaniel <munozda87@hotmail.com>
2015-10-02 13:08:50 -0300

    custom.js: Agregado velocidad a los link con .slow

--- public/js/custom.js
+++ public/js/custom.js
@@ -96,7 +96,7 @@ jQuery(function($){
 
 
 	/* ----------------------------------------------------------- */
-	/*  6. Project Counter
+	/*  6. Project Counter (No lo uso)
 	/* ----------------------------------------------------------- */
 
 	(function ($) {
@@ -451,11 +451,78 @@ jQuery(function($){
 
 	/* ----------------------------------------------------------- */
 	/*  15. MOBILE MENU ACTIVE CLOSE 
-	/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
 
 	$('.navbar-nav').on('click', 'li a', function() {
 	  $('.navbar-collapse').collapse('hide');
 	});
 
-	
+    /* ----------------------------------------------------------- */
+    /*  16. SCROLL LENTO PARA LOS QUE TENGAN LA CLASE <a class='slow'
+     /* ----------------------------------------------------------- */
+    $(function() {
+
+        function filterPath(string) {
+            return string
+                .replace(/^\//,'')
+                .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+                .replace(/\/$/,'');
+        }
+
+        var locationPath = filterPath(location.pathname);
+        var scrollElem = scrollableElement('html', 'body');
+
+        // Any links with hash tags in them (can't do ^= because of fully qualified URL potential)
+        $('a[class*="slow"]').each(function() {
+
+            // Ensure it's a same-page link
+            var thisPath = filterPath(this.pathname) || locationPath;
+            if (  locationPath == thisPath
+                && (location.hostname == this.hostname || !this.hostname)
+                && this.hash.replace(/#/,'') ) {
+
+                // Ensure target exists
+                var $target = $(this.hash), target = this.hash;
+                if (target) {
+
+                    // Find location of target
+                    var targetOffset = $target.offset().top;
+                    $(this).click(function(event) {
+
+                        // Prevent jump-down
+                        event.preventDefault();
+
+                        // Animate to target
+                        $(scrollElem).animate({scrollTop: targetOffset}, 500, function() {
+
+                            // Set hash in URL after animation successful
+                            location.hash = target;
+
+                        });
+                    });
+                }
+            }
+
+        });
+
+        // Use the first element that is "scrollable"  (cross-browser fix?)
+        function scrollableElement(els) {
+            for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+                var el = arguments[i],
+                    $scrollElement = $(el);
+                if ($scrollElement.scrollTop()> 0) {
+                    return el;
+                } else {
+                    $scrollElement.scrollTop(1);
+                    var isScrollable = $scrollElement.scrollTop()> 0;
+                    $scrollElement.scrollTop(0);
+                    if (isScrollable) {
+                        return el;
+                    }
+                }
+            }
+            return [];
+        }
+
+    });
 });
