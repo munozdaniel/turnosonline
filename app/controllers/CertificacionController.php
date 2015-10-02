1d4fdb5a7c1d6dd2ff4033b7e462bcc1397e3938
munozdaniel <munozda87@hotmail.com>
2015-10-02 13:17:03 -0300

    Certificacion: Nuevo Controlador y Vista

---
+++ app/controllers/CertificacionController.php
@@ -0,0 +1,18 @@
+<?php
+
+class CertificacionController extends \Phalcon\Mvc\Controller
+{
+    public function initialize()
+    {
+        $this->tag->setTitle('Certificación Negativa');
+        parent::initialize();
+
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
