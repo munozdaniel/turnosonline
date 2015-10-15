35d5fbfe567862588f2a10c44c6d5f3d13de20aa
munozdaniel <munozda87@hotmail.com>
2015-10-15 12:32:09 -0300

    TurnosController: Validando la solicitud de turno.

--- app/controllers/TurnosController.php
+++ app/controllers/TurnosController.php
@@ -27,58 +27,68 @@ class TurnosController extends ControllerBase
         $this->assets->collection('footerInline')
             ->addInlineJs("$(\".navbar-fixed-top\").addClass('past-main');");
     }
-    /*================================ index =======================================*/
+    /*================================ INVITADOS =======================================*/
     /**
      * Muestra el formulario para solicitar turno.
+     * 1. valida sus campos.
+     * 2. valida si esta dentro del periodo disponible para solicitar turnos.
+     * 3. verifica si los datos ingresados pertenecen a un afiliado de siprea y verifica si hay cupos disponibles
+     * 4. verifica que no haya solicitado otro turno en el periodo actual.
+     * 5.
      */
     public function indexAction()
     {
         $this->menuPpal();
-
         $turnosOnlineForm = new TurnosOnlineForm();
-
 
         if ($this->request->isPost()) {
             $data = $this->request->getPost();
-
             if ($turnosOnlineForm->isValid($data) != false) {
+                $razonNoDisponible = $this->verificarDisponibilidad();
+                if ($razonNoDisponible == "") {
 
-                //PeriodoSolicitud = Fechasturnos.
-                $solicitudTurno = new Solicitudturno();
-                //Los datos que estan comentados se setean en el futuro.
-                $legajo = $this->request->getPost('solicitudTurno_legajo', array('alphanum', 'trim'));
-                $nombre = $this->request->getPost('solicitudTurno_nom', array('striptags', 'string', 'upper'));
-                $apellido = $this->request->getPost('solicitudTurno_ape', array('striptags', 'string', 'upper'));
-                $nombreCompleto = $this->comprobarDatosEnSiprea($legajo, $apellido . " " . $nombre);
-                if ( $nombreCompleto != "") {
+                    $solicitudTurno = new Solicitudturno();
+                    //Los datos que estan comentados se setean en el futuro.
+                    $legajo = $this->request->getPost('solicitudTurno_legajo', array('alphanum', 'trim'));
+                    $nombre = $this->request->getPost('solicitudTurno_nom', array('striptags', 'string', 'upper'));
+                    $apellido = $this->request->getPost('solicitudTurno_ape', array('striptags', 'string', 'upper'));
+                    $nombreCompleto = $this->comprobarDatosEnSiprea($legajo, $apellido . " " . $nombre);
+                    if ($nombreCompleto != "") {
+                        $email = $this->request->getPost('solicitudTurno_email', array('email', 'trim'));
+                        if (!$this->tieneTurnoSolicitado($legajo, $email)) {
+                            $solicitudTurno->assign(array(
+                                'solicitudTurno_legajo' => $legajo,
+                                'solicitudTurno_nomApe' => $nombreCompleto,
+                                'solicitudTurno_documento' => $this->request->getPost('solicitudTurno_documento', array('alphanum', 'trim', 'string')),
+                                'solicitudTurno_email' => $email,
+                                'solicitudTurno_numTelefono' => $this->request->getPost('solicitudTurno_numTelefono', 'int'),
+                                'solicitudTurno_fechaPedido' => date('Y-m-d'),
+                                'solicitudTurno_estado' => "PENDIENTE",
+                                'solicitudTurno_nickUsuario' => "-",
+                                //'solicitudTurno_fechaProcesamiento'       => $this->request->getPost('cantidadTurnos','int'),
+                                'solicitudTurno_respuestaEnviada' => "NO",
+                                'solicitudTurno_respuestaChequeada' => 0,
+                                //'solicitudTurno_fechaRespuestaEnviada'    => 0,
+                                'solicitudTurno_montoMax' => 0,
+                                'solicitudTurno_montoPosible' => 0,
+                                'solicitudTurno_cantCuotas' => 0,
+                                'solicitudTurno_valorCuota' => 0,
+                                'solicitudTurno_observaciones' => "-",
+                                'solicitudTurno_manual' => 0,
+                            ));
+                            if ($solicitudTurno->save()) {
+                                $this->flash->message('exito', 'La configuración de los períodos se ha realizado satisfactoriamente.');
+                                $turnosOnlineForm->clear();
+                                $this->redireccionar('turnos/turnoSolicitado');
+                            }
+                        } else
+                            $this->flash->message('problema', 'SUS DATOS YA FUERON INGRESADO, NO PUEDE SACAR MÁS DE UN TURNO POR PERÍODO');
+                    } else
+                        $this->flash->message('problema', 'NO SE ENCUENTRA REGISTRADO EN EL SISTEMA, PARA MAS INFORMACIÓN DIRÍJASE A NUESTRAS OFICINAS.');
+                } else
+                    $this->flash->message('problema', $razonNoDisponible);
 
-                    $solicitudTurno->assign(array(
-                        'solicitudTurno_legajo'                     => $legajo,
-                        'solicitudTurno_nomApe'                     => $nombreCompleto,
-                        'solicitudTurno_documento'                  => $this->request->getPost('solicitudTurno_documento', array('alphanum', 'trim', 'string')),
-                        'solicitudTurno_email'                      => $this->request->getPost('solicitudTurno_email', array('email', 'trim')),
-                        'solicitudTurno_numTelefono'                => $this->request->getPost('solicitudTurno_numTelefono', 'int'),
-                        'solicitudTurno_fechaPedido'                => date('y-m-d'),
-                        'solicitudTurno_estado'                     => "PENDIENTE",
-                        'solicitudTurno_nickUsuario'                => "-",
-                        //'solicitudTurno_fechaProcesamiento'       => $this->request->getPost('cantidadTurnos','int'),
-                        'solicitudTurno_respuestaEnviada'           => "NO",
-                        'solicitudTurno_respuestaChequeada'         => 0,
-                        //'solicitudTurno_fechaRespuestaEnviada'    => 0,
-                        'solicitudTurno_montoMax'                 => 0,
-                        'solicitudTurno_montoPosible'             => 0,
-                        'solicitudTurno_cantCuotas'               => 0,
-                        'solicitudTurno_valorCuota'               => 0,
-                        'solicitudTurno_observaciones'            => "-",
-                        'solicitudTurno_manual'                   => 0,
-                    ));
-                    if ($solicitudTurno->save()) {
-                        $this->flash->message('exito','La configuración de los períodos se ha realizado satisfactoriamente.');
-                        $turnosOnlineForm->clear();
-                        $this->redireccionar('turnos/turnoSolicitado');
-                    }
-                }
-                $this->flash->message('problema', 'NO SE ENCUENTRA EN LA BASE DE DATOS DE SIPREA.');
+
             }
         }
         $this->view->formulario = $turnosOnlineForm;
@@ -96,7 +106,6 @@ class TurnosController extends ControllerBase
     private function comprobarDatosEnSiprea($legajo, $nombreCompleto)
     {
         try {
-            echo "$legajo - $nombreCompleto ";
             $sql = "SELECT AF.afiliado_legajo, AF.afiliado_apenom FROM siprea2.afiliados AS AF WHERE (AF.afiliado_apenom LIKE '%" . $nombreCompleto . "%') AND (AF.afiliado_legajo LIKE '%" . $legajo . "%');";
             $result = $this->dbSiprea->query($sql);
             if ($result->numRows() != 0) {
@@ -109,10 +118,56 @@ class TurnosController extends ControllerBase
         }
         return "";
     }
-    private function verificarDisponibilidad(){
-        $fechasTurnos = new Fechasturnos();
 
+    /**
+     * Se encarga de realizar 2 verificaciones:
+     * 1. que la fecha de hoy se encuentra entre el periodo ingresado por los supervisores.
+     * INICIO <= ACTUAL <= FINAL.
+     * 2. Que existan cupos disponibles.
+     * @return string muestra un mensaje si hubo problemas
+     */
+    private function verificarDisponibilidad()
+    {
+        $ultimo = (int)Fechasturnos::count() - 1;//Obtengo el ultimo indice
+        $fechasTurnos = Fechasturnos::find();//Obtengo todos las instancias de Fechasturnos.
+        if ($fechasTurnos[$ultimo]->fechasTurnos_inicioSolicitud <= date('Y-m-d')
+            && date('Y-m-d')<=$fechasTurnos[$ultimo]->fechasTurnos_finSolicitud
+        )
+            if ($fechasTurnos[$ultimo]->fechasTurnos_cantidadDeTurnos == $fechasTurnos[$ultimo]->fechasTurnos_cantidadAutorizados)
+                return "LO SENTIMOS, NO HAY CUPOS DISPONIBLES.";
+            else
+                return "";
+        else
+            return "NO ES POSIBLE SOLICITAR TURNOS EN LA FECHA ACTUAL. VERIFIQUE LAS FECHAS EN NUESTRA PAGINA WEB.";
     }
+
+    /**
+     * Verifica con los datos del afiliado si ya solicito un turno en este periodo.
+     *
+     * @return boolean devuelve si encontro o no.
+     */
+    private function tieneTurnoSolicitado($legajo, $email)
+    {
+        try {
+            $ultimo = (int)Fechasturnos::count() - 1;//Obtengo el ultimo indice
+            $fechasTurnos = Fechasturnos::find();//Obtengo todos las instancias de Fechasturnos.
+            $consulta = "SELECT * FROM solicitudTurno AS ST WHERE ((DATE(ST.solicitudTurno_fechaPedido) BETWEEN :inicioSolicitud: AND :finSolicitud:) AND (ST.solicitudTurno_legajo=:legajo:) AND (ST.solicitudTurno_email LIKE  :email:))";
+            $solicitudTurno = $this->modelsManager->executeQuery($consulta,
+                array(
+                    'inicioSolicitud' => $fechasTurnos[$ultimo]->fechasTurnos_inicioSolicitud,
+                    'finSolicitud' => $fechasTurnos[$ultimo]->fechasTurnos_finSolicitud,
+                    'legajo' => $legajo,
+                    'email' => $email));
+            //Si no encontro datos, es porque no solicito un turno en este periodo.
+            if (count($solicitudTurno) == 0)
+                return false;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            echo $e->getTraceAsString();
+        }
+        return true;
+    }
+
     /**
      * Recupera los datos del formulario TurnosOnlineForm y controla que exista en la bd Siprea, en caso
      * afirmativo se inserta en la bd impsorg_web, en la tabla SolicitudTurno.
@@ -123,12 +178,16 @@ class TurnosController extends ControllerBase
 
     }
 
-    /*================================ periodoSolicitud =======================================*/
+    /*================================ SUPERVISOR =======================================*/
     public function periodoSolicitudAction()
     {
         $this->view->formulario = new PeriodoSolicitudForm();
     }
 
+    /**
+     * Muestra el formulario de periodos, lo valida y guarda en la base de datos.
+     * Rol: Supervisor/Administrador
+     */
     public function guardarPeriodoSolicitudAction()
     {
         $this->view->pick('turnos/periodoSolicitud');
