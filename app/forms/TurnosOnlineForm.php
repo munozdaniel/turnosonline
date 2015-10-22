b46bef592db3173ace472aa6a5b54b65d08929b7
marciaAmanda <mpichun@imps.org.ar>
2015-10-22 11:59:06 -0300

    modifique el label de $telefono

--- app/forms/TurnosOnlineForm.php
+++ app/forms/TurnosOnlineForm.php
@@ -19,10 +19,10 @@ class TurnosOnlineForm  extends Form {
      */
     public function initialize($entity = null, $options = array())
     {
-        /*=================== Nombre y Apellido ==========================*/
-        $apellido = new Text("solicitudTurno_ape",array('style'=>'text-align:right !important;height: 40px !important;font-size: 18px;','placeholder'=>'APELLIDO'));
+        /*=================== Apellido ==========================*/
+        $apellido = new Text("solicitudTurno_ape",array('style'=>'text-align:right !important;height: 40px !important; width: 300px !important; font-size: 18px;','placeholder'=>'APELLIDO'));
 
-        $apellido->setLabel("<strong>(*)</strong> Apellido ");
+        $apellido->setLabel("<strong>(*)</strong> Apellido:");
         $apellido->setFilters(array('string'));
         $apellido->addValidators(
             array(
@@ -33,23 +33,23 @@ class TurnosOnlineForm  extends Form {
                 ),
                 new StringLength(array(
                     'min' => 4,
-                    'messageMinimum' => 'Nombre demasiado corto',
+                    'messageMinimum' => 'Apellido demasiado corto.',
                     'max' => 30,
-                    'messageMaximun' => 'Nombre demasiado largo',
+                    'messageMaximun' => 'Apellido demasiado largo.',
                 )),
             )
         );
         $this->add($apellido);
-        /*=================== Nombre y Apellido ==========================*/
-        $nombre = new Text("solicitudTurno_nom",array('style'=>'text-align:right !important;height: 40px !important;font-size: 18px;','placeholder'=>'NOMBRE'));
+        /*=================== Nombre ==========================*/
+        $nombre = new Text("solicitudTurno_nom",array('style'=>'text-align:right !important;height: 40px !important;width: 300px !important;font-size: 18px;','placeholder'=>'NOMBRE'));
 
-        $nombre->setLabel("<strong>(*)</strong> Nombre ");
+        $nombre->setLabel("<strong>(*)</strong> Nombre: ");
         $nombre->setFilters(array('string'));
         $nombre->addValidators(
             array(
                 new PresenceOf(
                     array(
-                        'message' => 'Ingrese su <strong>Apellido y Nombre</strong>.'
+                        'message' => 'Ingrese su <strong>Nombre</strong>.'
                     )
                 ),
                 new StringLength(array(
@@ -62,9 +62,9 @@ class TurnosOnlineForm  extends Form {
         );
         $this->add($nombre);
         /*=================== Nro Legajo ==========================*/
-        $legajo = new Text("solicitudTurno_legajo",array('style'=>'text-align:right !important;height: 40px !important;font-size: 18px;','placeholder'=>'LEGAJO'));
+        $legajo = new Text("solicitudTurno_legajo",array('style'=>'text-align:right !important;height: 40px !important;width: 300px !important;font-size: 18px;','placeholder'=>'LEGAJO'));
 
-        $legajo->setLabel("<strong>(*)</strong> Legajo ");
+        $legajo->setLabel("<strong>(*)</strong> Legajo: ");
         $legajo->setFilters(array('int'));
         $legajo->addValidators(
             array(
@@ -81,16 +81,16 @@ class TurnosOnlineForm  extends Form {
                 new NumberValidator(),
                 new StringLength(array(
                     'min' => 4,
-                    'messageMinimum' => 'Minimo 4 digitos',
+                    'messageMinimum' => 'Minimo 4 digitos.',
                     'max' => 12,
-                    'messageMaximun' => 'Maximo 12 digitos',
+                    'messageMaximun' => 'Maximo 12 digitos.',
                 )),
             )
         );
         $this->add($legajo);
         /*=================== Nro Documento ==========================*/
-        $dni = new Text("solicitudTurno_documento",array('style'=>'text-align:right !important;height: 40px !important;font-size: 18px;','placeholder'=>'NRO DOCUMENTO'));
-        $dni->setLabel("<strong>(*)</strong> Nro Documento ");
+        $dni = new Text("solicitudTurno_documento",array('style'=>'text-align:right !important;height: 40px !important;width: 300px !important;font-size: 18px;','placeholder'=>'NRO DOCUMENTO'));
+        $dni->setLabel("<strong>(*)</strong> Nro Documento: ");
         $dni->setFilters(array('int'));
         $dni->addValidators(
             array(
@@ -107,16 +107,16 @@ class TurnosOnlineForm  extends Form {
                 new NumberValidator(),
                 new StringLength(array(
                     'min' => 4,
-                    'messageMinimum' => 'Minimo 4 digitos',
+                    'messageMinimum' => 'Minimo 4 digitos.',
                     'max' => 12,
-                    'messageMaximun' => 'Maximo 12 digitos',
+                    'messageMaximun' => 'Maximo 12 digitos.',
                 ))
             )
         );
         $this->add($dni);
         /*=================== Fecha Nacimiento ==========================*/
-        $fechaNacimiento= new Date('solicitudTurno_fechaNacimiento',array('style'=>'text-align:right !important'));
-        $fechaNacimiento->setLabel('<strong>(*)</strong> Fecha Nacimiento');
+        $fechaNacimiento= new Date('solicitudTurno_fechaNacimiento',array('style'=>'text-align:right !important;width: 300px !important;'));
+        $fechaNacimiento->setLabel('<strong>(*)</strong> Fecha Nacimiento:');
         $fechaNacimiento->addValidators(array(
             new PresenceOf(array(
                 'message' => 'Ingrese la <strong>Fecha de Nacimiento</strong>.'
@@ -124,12 +124,16 @@ class TurnosOnlineForm  extends Form {
         ));
         $this->add($fechaNacimiento);
         /*=================== Nro Legajo ==========================*/
-        $telefono = new Text("solicitudTurno_numTelefono",array('style'=>'text-align:right !important;height: 40px !important;font-size: 18px;','placeholder'=>'TELEFONO'));
-
-        $telefono->setLabel("Telefono ");
+        $telefono = new Text("solicitudTurno_numTelefono",array('style'=>'text-align:right !important;height: 40px !important;width: 300px !important;font-size: 18px;','placeholder'=>'TELEFONO'));
+        $telefono->setLabel("<strong>(*)</strong> N&uacute;mero de telefono/celular (c&oacute;digo de &aacute;rea y n&uacute;mero):");
         $telefono->setFilters(array('int'));
         $telefono->addValidators(
             array(
+                new PresenceOf(
+                    array(
+                        'message' => 'Ingrese el <strong>n&uacute;mero de telefono/celular.</strong>.'
+                    )
+                ),
                 new Numericality(
                     array(
                         'message' => 'El <strong>Telefono</strong> debe ser un número sin puntos ni coma.'
@@ -137,56 +141,56 @@ class TurnosOnlineForm  extends Form {
                 ),
                 new NumberValidator(),
                 new StringLength(array(
-                    'min' => 4,
-                    'messageMinimum' => 'Minimo 4 digitos',
-                    'max' => 12,
-                    'messageMaximun' => 'Maximo 12 digitos',
+                    'min' => 6,
+                    'messageMinimum' => 'Minimo 6 digitos.',
+                    'max' => 14,
+                    'messageMaximun' => 'Maximo 14 digitos.',
                 )),
             )
         );
         $this->add($telefono);
         /*=================== Correo Electronico ==========================*/
-        $email = new \Phalcon\Forms\Element\Email('solicitudTurno_email', array('style'=>'text-align:right !important;height: 40px !important;font-size: 18px;',
+        $email = new \Phalcon\Forms\Element\Email('solicitudTurno_email', array('style'=>'text-align:right !important;height: 40px !important;width: 300px !important;font-size: 18px;',
             'placeholder' => 'EMAIL'
         ));
-        $email->setLabel('<strong>(*)</strong> Email');
+        $email->setLabel('<strong>(*)</strong> Email:');
         $email->addValidators(array(
             new PresenceOf(array(
-                'message' => 'El Email es requerido'
+                'message' => 'El Email es requerido.'
             )),
             new Email(array(
-                'message' => 'El Email no es Valido.'
+                'message' => 'El Email no es válido.'
             ))
         ));
 
         $this->add($email);
         /*=================== Repita su Correo Electronico ==========================*/
-        $confirmarEmail = new \Phalcon\Forms\Element\Email('emailRepetido', array('style'=>'text-align:right !important;height: 40px !important;font-size: 18px;',
+        $confirmarEmail = new \Phalcon\Forms\Element\Email('emailRepetido', array('style'=>'text-align:right !important;height: 40px !important;width: 300px !important;font-size: 18px;',
             'placeholder' => 'REPITA EL EMAIL'
         ));
 
-        $confirmarEmail->setLabel('<strong>(*)</strong> Repetir Email');
+        $confirmarEmail->setLabel('<strong>(*)</strong> Repita el Email:');
         $confirmarEmail->addValidators(array(
             new PresenceOf(array(
                 'message' => 'El Email es requerido'
             )),
             new Email(array(
-                'message' => 'El Email no es valido.'
+                'message' => 'El Email no es válido.'
             )),
             new ComprobarEmailValidator(array('email'=>$email))
         ));
 
         $this->add($confirmarEmail);
-
     }
-    /**
-     * Prints messages for a specific element
-     */
+
+    //muestra un mensaje por cada elemento
     public function messages($name)
     {
         $cadena= "";
-        if ($this->hasMessagesFor($name)) {
-            foreach ($this->getMessagesFor($name) as $message) {
+        if ($this->hasMessagesFor($name))
+        {
+            foreach ($this->getMessagesFor($name) as $message)
+            {
                 $cadena.= "<div class='problema'>".$message ."</div>";//para mostrar con tooltip
             }
         }
