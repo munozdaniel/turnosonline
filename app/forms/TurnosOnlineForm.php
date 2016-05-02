eaf2221a687a510bb741e4f366142a6cc10e56b6
munozdaniel <munozda87@hotmail.com>
2016-05-02 08:28:54 -0300

    TurnoOnlineForm: formulario para turnos online y manual.

--- app/forms/TurnosOnlineForm.php
+++ app/forms/TurnosOnlineForm.php
@@ -13,25 +13,19 @@ use Phalcon\Validation\Validator\Numericality;
 use Phalcon\Validation\Validator\StringLength as StringLength;
 use Phalcon\Validation\Validator\Email;
 
-class TurnosOnlineForm  extends Form {
+class TurnosOnlineForm extends Form
+{
     /**
      * Initialize the products form
      */
     public function initialize($entity = null, $options = array())
     {
         /*=================== Apellido ==========================*/
-        $disable = array();
-        $disable['key']='';
-        $disable['value']='';
-        if(isset($options['disabled'])){
-            $disable['key']='disabled';
-            $disable['value']='true';
-        }
         $apellido = new Text("solicitudTurno_ape",
-            array('style'=>'text-align:right !important; font-size: 18px;',
-                'placeholder'=>'APELLIDO',
-                'class'=>'form-control',
-                'required'=>'true','autocomplete'=>'off',$disable['key']=>$disable['value']));
+            array('style' => 'text-align:right !important; font-size: 18px;',
+                'placeholder' => 'APELLIDO',
+                'class' => 'form-control',
+                'autocomplete' => 'off', 'required' => true));
         $apellido->setLabel("<strong style='color: red'>*</strong> Apellido:");
         $apellido->setFilters(array('string'));
         $apellido->addValidators(
@@ -52,10 +46,10 @@ class TurnosOnlineForm  extends Form {
         $this->add($apellido);
         /*=================== Nombre ==========================*/
         $nombre = new Text("solicitudTurno_nom",
-            array('style'=>'text-align:right !important;font-size: 18px;',
-                'placeholder'=>'NOMBRE',
-                'class'=>'form-control',
-                'required'=>'true','autocomplete'=>'off',$disable['key']=>$disable['value']));
+            array('style' => 'text-align:right !important;font-size: 18px;',
+                'placeholder' => 'NOMBRE',
+                'class' => 'form-control',
+                'autocomplete' => 'off', 'required' => true));
 
         $nombre->setLabel("<strong style='color: red'>*</strong> Nombre: ");
         $nombre->setFilters(array('string'));
@@ -77,10 +71,10 @@ class TurnosOnlineForm  extends Form {
         $this->add($nombre);
         /*=================== Nro Legajo ==========================*/
         $legajo = new Text("solicitudTurno_legajo",
-            array('style'=>'text-align:right !important;font-size: 18px;',
-                'placeholder'=>'LEGAJO',
-                'class'=>'form-control',
-                'required'=>'true','autocomplete'=>'off',$disable['key']=>$disable['value']));
+            array('style' => 'text-align:right !important;font-size: 18px;',
+                'placeholder' => 'LEGAJO',
+                'class' => 'form-control',
+                'autocomplete' => 'off', 'required' => true));
 
         $legajo->setLabel("<strong style='color: red'>*</strong> Legajo: ");
         $legajo->setFilters(array('int'));
@@ -108,10 +102,10 @@ class TurnosOnlineForm  extends Form {
         $this->add($legajo);
         /*=================== Nro Documento ==========================*/
         $dni = new Text("solicitudTurno_documento",
-            array('style'=>'text-align:right !important;font-size: 18px;',
-            'placeholder'=>'NRO DOCUMENTO',
-            'class'=>'form-control',
-            'required'=>'true','autocomplete'=>'off',$disable['key']=>$disable['value']));
+            array('style' => 'text-align:right !important;font-size: 18px;',
+                'placeholder' => 'NRO DOCUMENTO',
+                'class' => 'form-control',
+                'autocomplete' => 'off', 'required' => true));
         $dni->setLabel("<strong style='color: red'>*</strong> Nro Documento: ");
         $dni->setFilters(array('int'));
         $dni->addValidators(
@@ -137,10 +131,9 @@ class TurnosOnlineForm  extends Form {
         );
         $this->add($dni);
         /*=================== Fecha Nacimiento ==========================*/
-        $fechaNacimiento= new Date('solicitudTurno_fechaNacimiento',
-            array('style'=>'text-align:right !important;font-size: 18px;',
-                'class'=>'form-control',
-                'required'=>'true',$disable['key']=>$disable['value']));
+        $fechaNacimiento = new Date('solicitudTurno_fechaNacimiento',
+            array('style' => 'text-align:right !important;font-size: 18px;',
+                'class' => 'form-control', 'required' => true));
         $fechaNacimiento->setLabel("<strong style='color: red'>*</strong> Fecha Nacimiento:");
         $fechaNacimiento->addValidators(array(
             new PresenceOf(array(
@@ -148,12 +141,12 @@ class TurnosOnlineForm  extends Form {
             ))
         ));
         $this->add($fechaNacimiento);
-        /*=================== Nro Legajo ==========================*/
+        /*=================== Nro Telefono ==========================*/
         $telefono = new Text("solicitudTurno_numTelefono",
-            array('style'=>'text-align:right !important;font-size: 18px;',
-                'placeholder'=>'TELEFONO',
-                'class'=>'form-control',
-                'required'=>'true','autocomplete'=>'off',$disable['key']=>$disable['value']));
+            array('style' => 'text-align:right !important;font-size: 18px;',
+                'placeholder' => 'TELEFONO',
+                'class' => 'form-control',
+                'autocomplete' => 'off', 'required' => true));
         $telefono->setLabel("<strong style='color: red'>*</strong> N&uacute;mero de telefono/celular:");
         $telefono->setFilters(array('int'));
         $telefono->addValidators(
@@ -178,52 +171,66 @@ class TurnosOnlineForm  extends Form {
             )
         );
         $this->add($telefono);
-        /*=================== Correo Electronico ==========================*/
-        $email = new \Phalcon\Forms\Element\Email('solicitudTurno_email',
-            array('style'=>'text-align:right !important;font-size: 18px;',
-            'placeholder' => 'EMAIL',
-                'class'=>'form-control',
-                'required'=>'true','autocomplete'=>'off',$disable['key']=>$disable['value']));
-        $email->setLabel("<strong style='color: red'>*</strong> Email:");
-        $email->addValidators(array(
-            new PresenceOf(array(
-                'message' => 'El email es requerido.'
-            )),
-            new Email(array(
-                'message' => 'El email no es válido.'
-            ))
-        ));
 
+        /*=================== Correo Electronico ==========================*/
+        //Si es manual, el email es opcional.
+        if (!isset($options['manual'])) {
+            $requerido = "required";
+            $asterisco = "<strong style='color: red'>*</strong>";
+        } else {
+            $asterisco = "";
+            $requerido = "unrequired";//En las solicitudes manuales el correo no es requerido.
+        }
+        $email = new \Phalcon\Forms\Element\Email('solicitudTurno_email',
+            array('style' => 'text-align:right !important;font-size: 18px;',
+                'placeholder' => 'EMAIL',
+                'class' => 'form-control',
+                'autocomplete' => 'off', $requerido => true));
+        $email->setLabel("$asterisco Email:");
+        if (!isset($options['manual'])) {
+
+            $email->addValidators(array(
+                new PresenceOf(array(
+                    'message' => 'El email es requerido.'
+                )),
+                new Email(array(
+                    'message' => 'El email no es válido.'
+                ))
+            ));
+        }
         $this->add($email);
         /*=================== Repita su Correo Electronico ==========================*/
-        $confirmarEmail = new \Phalcon\Forms\Element\Email('emailRepetido',
-            array('style'=>'text-align:right !important;font-size: 18px;',
-            'placeholder' => 'REPITA EL EMAIL',
-                'class'=>'form-control',
-                'required'=>'true','autocomplete'=>'off',$disable['key']=>$disable['value']));
-        $confirmarEmail->setLabel("<strong style='color: red'>*</strong> Repita el Email:");
-        $confirmarEmail->addValidators(array(
-            new PresenceOf(array(
-                'message' => 'El email es requerido'
-            )),
-            new Email(array(
-                'message' => 'El email no es válido.'
-            )),
-            new ComprobarEmailValidator(array('email'=>$email))
-        ));
+        //Si es Manual no es necesario repetir el email.
+        if (!isset($options['manual'])) {
 
-        $this->add($confirmarEmail);
+
+            $confirmarEmail = new \Phalcon\Forms\Element\Email('emailRepetido',
+                array('style' => 'text-align:right !important;font-size: 18px;',
+                    'placeholder' => 'REPITA EL EMAIL',
+                    'class' => 'form-control',
+                    'autocomplete' => 'off', 'required' => true));
+            $confirmarEmail->setLabel("<strong style='color: red'>*</strong> Repita el Email:");
+            $confirmarEmail->addValidators(array(
+                new PresenceOf(array(
+                    'message' => 'El email es requerido'
+                )),
+                new Email(array(
+                    'message' => 'El email no es válido.'
+                )),
+                new ComprobarEmailValidator(array('email' => $email))
+            ));
+
+            $this->add($confirmarEmail);
+        }
     }
 
     //muestra un mensaje por cada elemento
     public function messages($name)
     {
-        $cadena= "";
-        if ($this->hasMessagesFor($name))
-        {
-            foreach ($this->getMessagesFor($name) as $message)
-            {
-                $cadena.= "<div class='problema'>".$message ."</div>";//para mostrar con tooltip
+        $cadena = "";
+        if ($this->hasMessagesFor($name)) {
+            foreach ($this->getMessagesFor($name) as $message) {
+                $cadena .= "<div class='problema'>" . $message . "</div>";//para mostrar con tooltip
             }
         }
         return $cadena;
