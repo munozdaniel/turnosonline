352fc4c5f4bd133d9b06879d4ed0dcf1fcde647e
marciaAmanda <mpichun@imps.org.ar>
2016-05-19 16:28:17 -0300

    agregue los actions solicitudesPorPeriodo y solicitudesPorPeriodoAjax

--- app/controllers/TurnosController.php
+++ app/controllers/TurnosController.php
@@ -634,7 +634,6 @@ class TurnosController extends ControllerBase
         $retorno['data'] = $datos;
         echo json_encode($retorno);
         return;
-
     }
 
     /**
@@ -1804,10 +1803,91 @@ class TurnosController extends ControllerBase
         $retorno['success'] = true;
         echo json_encode($retorno);
         return;
-
-
     }
 
+    public function solicitudesPorPeriodoAction()
+    {
+        $this->importarDataTables();
+        $idPeriodo = $this->request->get('idP');
+        $periodo = Fechasturnos::findFirstByFechasTurnos_id($idPeriodo);
+
+        if ($periodo)
+        {
+            $this->view->ffInicioSol = date('d/m/Y',strtotime($periodo->fechasTurnos_inicioSolicitud));
+            $this->view->ffFinSol = date('d/m/Y',strtotime($periodo->fechasTurnos_finSolicitud));
+            $this->view->ffInicioAtencion = date('d/m/Y',strtotime($periodo->fechasTurnos_diaAtencion));
+            $this->view->ffFinAtencion= date('d/m/Y',strtotime($periodo->fechasTurnos_diaAtencionFinal));
+            $this->view->idP = $idPeriodo;
+        }
+    }
+
+    public function solicitudesPorPeriodoAjaxAction()
+    {
+        $this->view->disable();
+        $retorno = array();
+        $datos = array();
+
+        $idP = $this->request->getPost('idP');
+
+            $solicitudes = Solicitudturno::find(array('solicitudTurnos_fechasTurnos = :fechasTurnos_id:',
+                'bind' => array('fechasTurnos_id' => $idP),
+                'order' => 'solicitudTurno_id ASC'));
+
+            foreach ($solicitudes as $unaSolicitud)
+            {
+                    $item = array();
+
+                    //0 ID: Se utiliza para aceptar/cancelar asistencia
+                    $item[] = $unaSolicitud->getSolicitudturnoId();
+                    //1 Tipo de Turno: para pintar la fila de rojo
+                    $item[] = $unaSolicitud->getSolicitudturnoEstadoasistenciaid();
+                    //2 Codigo
+                    $item[] = $unaSolicitud->getSolicitudturnoCodigo();
+                    //3 Afiliado
+                    $item[] = '<h4><ins>' . $unaSolicitud->getSolicitudturnoLegajo() . ' </ins></h4>' . $unaSolicitud->getSolicitudturnoNomape();
+                    //4 Email/Telefono
+                    if ($unaSolicitud->getSolicitudturnoEmail() == NULL || trim($unaSolicitud->getSolicitudturnoEmail()) == "")
+                        $email = '';
+                    else
+                        $email = "" . $unaSolicitud->getSolicitudturnoEmail();
+                    $item[] = "<i class='fa fa-envelope-o'></i> " . $email . " <br> <i class='fa fa-phone-square'></i> " . $unaSolicitud->getSolicitudturnoNumtelefono();
+
+                    //5 Usuario
+                    $item[] =$unaSolicitud->getSolicitudturnoNickusuario();
+                    //6 Estado Deuda: Autorizado, denegado, denegado por falta de turno
+                    $item[] = $unaSolicitud->getSolicitudturnoEstado();
+                    //7 Observaciones
+                    $item[] = $unaSolicitud->getSolicitudturnoObservaciones();
+
+
+                    switch ($unaSolicitud->getSolicitudturnoEstadoasistenciaid())
+                    {
+                        case 1:
+                            $estadoAsistencia = 'EN ESPERA';
+                            break;
+                        case 2:
+                            $estadoAsistencia = "CONFIRMADO";
+                            break;
+                        case 3:
+                            $estadoAsistencia = "PLAZO VENCIDO";
+                            break;
+                        case 4:
+                            $estadoAsistencia ="CANCELADO";
+                            break;
+                    }
+
+                    //8 Estado Asistencia
+                    $item[] = $estadoAsistencia;
+
+                    //9 tipo solicitud
+                    $item[] = $unaSolicitud->getTipoturno()->getTipoturnoNombre();
+                    $datos[] = $item;
+            }
+
+        $retorno['data'] = $datos;
+        echo json_encode($retorno);
+        return;
+    }
 }
 
 
