1f33bdcc3358faca06997552f5e1fd65cac4557f
munozdaniel <munozda87@hotmail.com>
2016-07-28 09:15:05 -0300

    host bd gestionUsuario: ip publica y puerto

--- app/config/config.php
+++ app/config/config.php
@@ -13,7 +13,7 @@ return new \Phalcon\Config(array(
     ),
     'gestionusuarios' => array(
         'adapter'     => 'Mysql',
-        'host'        => '192.168.42.11',
+        'host'        => '201.251.125.209:4880',
         'username'    => 'root',
         'password'    => 'infoimps',
         'dbname'      => 'gestionusuarios',
